00dabcce87113556304e116f0fbdfda403023ddf
bowen163 <763893371@qq.com>
2019-09-04 11:26:32 +0800

    Update main.swift

--- test Swift/main.swift
+++ test Swift/main.swift
@@ -7,7 +7,12 @@
 //
 
 import Foundation
-typealias feet = String
-var name:feet = "2345"
-var feather = "删都删不掉的"
+var name = "2345"
+var feather = "\n\"删都删不掉的\""
 print("\(name)浏览器\(feather)")
+var a = 19
+var b = 34
+print("a+b=\0\(a + b)\na*b=\0\(a * b)");
+a+=1;
+print("a加1的值为\(a)")
+
